a158f2d74624e5982d8250c37675010b1736cdfc
bartek <bartek@DESKTOP-RF2EMT2>
2016-09-06 22:04:18 +0200

    init abstractfactory

---
+++ DesignPattern/AbstractFactory/DyskDlaLaptopa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class DyskDlaLaptopa : IDysk
+    {
+        String name = "DyskDlaLaptopa";
+        public IDysk dawajDysk()
+        {
+            Console.WriteLine("Dodaje: " + name);
+            return new DyskDlaLaptopa();
+        }
+    }
+}
---
+++ DesignPattern/AbstractFactory/IFabrykaPodzespolowKomputerowych.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    interface IFabrykaPodzespolowKomputerowych
+    {
+        IDysk produkujemyDysk();
+        IGrafika produkujemyGrafike();
+        IRam produkujemyRam();
+        IProcesor produkujemyProcesor();
+        IChlodzenie produkujemyChlodzenie();
+    }
+}
