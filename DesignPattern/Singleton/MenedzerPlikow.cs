4030dc043bc28e7b7a076fefe513329277b3a3ce
bartosz.szewczyk <bartosz.szewczyk@ATJ340BWR2302>
2016-09-09 14:32:44 +0200

    singleton

---
+++ DesignPattern/Singleton/MenedzerPlikow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class MenedzerPlikow
+    {
+        private static MenedzerPlikow instancja = null;
+        private int identyfikator;
+
+        private MenedzerPlikow()
+        {
+            Random random = new Random();
+            identyfikator = random.Next(10);
+        }
+
+        //-------------------------------------------------------------------------------------------#1 sposob
+        public static MenedzerPlikow dawajMenedzer()
+        {
+            if (instancja == null)
+            {
+                instancja = new MenedzerPlikow();
+                return instancja;
+            }
+            Console.WriteLine("Menedzer juz zostal wczesniej utworzony!");
+            return instancja;
+        }
+
+        public void about()
+        {
+            Console.WriteLine("Identyfiaktor = " + identyfikator);
+        }
+
+        /*-------------------------------------------------------------------------------------------#2 sposob
+        // nie inicjujemy tutan nullem, instancja juz od poczatku posiada wartosc
+
+        private static readonly MenedzerPlikow instancja = new MenedzerPlikow();
+        public static MenedzerPlikow dawajMenedzer(){
+        return instancja;
+        }
+        */
+
+    }
+}
--- DesignPattern/Singleton/Program.cs
+++ DesignPattern/Singleton/Program.cs
@@ -10,6 +10,15 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+
+            MenedzerPlikow[] tab = new MenedzerPlikow[3];
+            tab[0] = MenedzerPlikow.dawajMenedzer();
+            tab[1] = MenedzerPlikow.dawajMenedzer();
+            tab[2] = MenedzerPlikow.dawajMenedzer();
+
+            for (int i = 0; i < 3; i++)
+                tab[i].about();
+            Console.ReadLine();
         }
     }
 }
